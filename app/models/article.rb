15c8205790d4d04ab8ec12bf9463d06c5f8266dd
Onur YONTAR <onuryontar@gmail.com>
2024-08-14 21:44:37 +0300

    adjust

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  #validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 1 }
 end
