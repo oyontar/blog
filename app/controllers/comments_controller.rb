0ef9089dd35400975b72ee90099b7f124ac4bd40
Onur YONTAR <onuryontar@gmail.com>
2024-08-15 00:18:15 +0300

    username
    user.rb ve article.rb değiştirildi has_many

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
   
-  http_basic_authenticate_with name: "onur", password: "1111", only: :destroy
-  
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
